a97d85fd99ef7a605ecaf045659d95e9e565d694
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-11-02 14:27:31 +0530

    maping articles

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 
 class NavItems extends Component {
   render() {
-    let { title } = this.props;
+    let { title, description, image } = this.props;
     return (
       <div>
         <div class="card" style={{ width: "18rem" }}>
-          <img src="..." class="card-img-top" alt="..." />
+          <img src={image} class="card-img-top" alt="..." />
           <div class="card-body">
             <h5 class="card-title">{title}</h5>
-            <p class="card-text">Description</p>
+            <p class="card-text">{description}</p>
             <a href="#" class="btn btn-primary">
               Go somewhere
             </a>
