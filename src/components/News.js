7138d4cdf1f3c46f2a9c1047714693d9a904c81f
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-11-02 13:27:44 +0530

    adding newsitems

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-
+import NewsItems from "./NewsItems";
 class News extends Component {
   render() {
     return (
       <div>
-        <h1>This is breaking news</h1>
+        <NewsItems />
       </div>
     );
   }
---
+++ src/components/NewsItems.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class NavItems extends Component {
+  render() {
+    return (
+      <div>
+        <div class="card" style={{ width: "18rem" }}>
+          <img src="..." class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Description</p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NavItems;
