4bad0d3cef204ae4d540d0045b39e25f59619f56
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-11-02 13:47:47 +0530

    adding card

--- src/components/News.js
+++ src/components/News.js
@@ -3,8 +3,25 @@ import NewsItems from "./NewsItems";
 class News extends Component {
   render() {
     return (
-      <div>
-        <NewsItems />
+      <div className="container my-4">
+        <h1 className="text-center">New Application</h1>
+        <div className="row">
+          <div className="col-md-3 my-4">
+            <NewsItems title={"FirstNews"} />
+          </div>
+
+          <div className="col-md-3 my-4">
+            <NewsItems title={"SecondNews"} />
+          </div>
+
+          <div className="col-md-3 my-4">
+            <NewsItems title={"ThirdNews"} />
+          </div>
+
+          <div className="col-md-3 my-4">
+            <NewsItems title={"FourthNews"} />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 
 class NavItems extends Component {
   render() {
+    let { title } = this.props;
     return (
       <div>
         <div class="card" style={{ width: "18rem" }}>
           <img src="..." class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">{title}</h5>
             <p class="card-text">Description</p>
             <a href="#" class="btn btn-primary">
               Go somewhere
