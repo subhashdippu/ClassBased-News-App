f564e0be06681c371b76468d79afe41c3163ccd9
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-11-02 13:22:52 +0530

    adding news

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
+import News from "./components/News";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <News />
       </div>
     );
   }
---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class News extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is breaking news</h1>
+      </div>
+    );
+  }
+}
+
+export default News;
