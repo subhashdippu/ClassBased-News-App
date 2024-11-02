6517f0c19c54fe3e07b1e9827924c2fa1249394b
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-11-02 13:16:43 +0530

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
-    return <div>Hello Subhash</div>;
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
   }
 }
 
